30b85039cdb420f1e7af29607670cecf54297fde
Patrick Morton <patrickjamesmorton@gmail.com>
2024-05-23 16:26:22 +0100

    make deployment process infer values from CDK context

--- infrastructure/src/infrastructure.ts
+++ infrastructure/src/infrastructure.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { InfrastructureStack } from './infrastructure-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { InfrastructureStack } from "./infrastructure-stack";
 
-const env = process.env.ENV || "dev";
-
-const stackName = env === 'dev' ? `TodosStack-${env}` : `TodosStack`;
+const env = process.env.ENV ?? "dev";
+const stackName = env === "dev" ? `TodosStack-${env}` : `TodosStack`;
 
 const app = new cdk.App();
 new InfrastructureStack(app, stackName, {
@@ -15,11 +14,11 @@ new InfrastructureStack(app, stackName, {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '011624951925', region: 'eu-west-2' },
+  // env: { account, region },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
